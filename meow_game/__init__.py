581514fb33da0aae129aefcc3308072d47b8ba00
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-03-15 07:42:27 +0400

    added random mouse spawning

--- meow_game/__init__.py
+++ meow_game/__init__.py
@@ -1,7 +1,11 @@
+from random import randint
+
 import pygame
 from pygame import Vector2
+from pygame.sprite import Group
 
 from meow import Meow, MEOW_LAUNCH_STRENGTH
+from mouse import Mouse
 
 FPS = 60
 
@@ -12,45 +16,70 @@ def main():
     win_size = Vector2(win.get_size())
     clock = pygame.time.Clock()
 
+    # Custom events
+    spawn_mouse_event = pygame.USEREVENT + 1
+
+    # Game objects
     meow = Meow(win_size / 2)
+    mice: Group[Mouse] = Group()  # type: ignore
+
+    # Spawn a mouse every 3 seconds
+    pygame.time.set_timer(spawn_mouse_event, 3000)
+
+    meow_can_move = True
 
     while True:
         dt = clock.tick(FPS) / 1000
 
         # Event handling
         for ev in pygame.event.get():
-            match ev.type:
-                case pygame.QUIT:
-                    return
+            if ev.type == pygame.QUIT:
+                return
 
-                case pygame.MOUSEBUTTONDOWN:
-                    mouse_pos = Vector2(pygame.mouse.get_pos())
-                    meow_pos = Vector2(meow.rect.center)
-                    dir = (mouse_pos - meow_pos).normalize()
-                    meow.velocity = dir * MEOW_LAUNCH_STRENGTH
+            elif ev.type == pygame.MOUSEBUTTONDOWN and meow_can_move:
+                mouse_pos = Vector2(pygame.mouse.get_pos())
+                meow_pos = Vector2(meow.rect.center)  # type: ignore
+
+                dir = (mouse_pos - meow_pos).normalize()
+                meow.velocity = dir * MEOW_LAUNCH_STRENGTH
+
+                meow_can_move = False
+
+            elif ev.type == spawn_mouse_event:
+                new_x = randint(0, int(win_size.x - 100)) + 50
+                new_y = randint(0, int(win_size.y - 100)) + 50
+                mouse = Mouse(Vector2(new_x, new_y))
+                mice.add(mouse)
 
         # Update game objects
         meow.update(dt)
+        mice.update(dt)
 
         # Edge collisions
 
-        if meow.rect.left < 0:
+        if meow.rect.left < 0:  # type: ignore
             meow.velocity.x *= -1
-            meow.rect.left = 0
-        elif meow.rect.right > win_size.x:
+            meow.rect.left = 0  # type: ignore
+        elif meow.rect.right > win_size.x:  # type: ignore
             meow.velocity.x *= -1
-            meow.rect.right = win_size.x
+            meow.rect.right = win_size.x  # type: ignore
 
-        if meow.rect.top < 0:
+        if meow.rect.top < 0:  # type: ignore
             meow.velocity.y *= -1
-            meow.rect.top = 0
-        elif meow.rect.bottom > win_size.y:
+            meow.rect.top = 0  # type: ignore
+        elif meow.rect.bottom > win_size.y:  # type: ignore
             meow.velocity.y *= -1
-            meow.rect.bottom = win_size.y
+            meow.rect.bottom = win_size.y  # type: ignore
 
         # Draw game objects
+
         win.fill("black")
+
         meow.show(win)
+
+        for mouse in mice:
+            mouse.show(win)
+
         pygame.display.flip()
 
 
--- meow_game/mouse.py
+++ meow_game/mouse.py
@@ -4,30 +4,25 @@ import pygame
 from pygame import Vector2
 from pygame.transform import scale_by
 
-MEOW_IMAGE_PATH = Path(__file__).parent.parent / "assets" / "meow.png"
-MEOW_LAUNCH_STRENGTH = 1500
-MEOW_VELOCITY_DAMPING = 0.02
+MOUSE_IMAGE_PATH = Path(__file__).parent.parent / "assets" / "mouse.png"
 
 
-class Meow(pygame.sprite.Sprite):
+class Mouse(pygame.sprite.Sprite):
     """
-    A meow. Also the main character!
+    A mouse. Also the antagonist!
     """
 
     def __init__(self, position: Vector2):
         super().__init__()
 
-        self._image = pygame.image.load(MEOW_IMAGE_PATH)
-        self._image = scale_by(self._image, 2)
+        self._image = pygame.image.load(MOUSE_IMAGE_PATH)
+        self._image = scale_by(self._image, 1.5)
 
         self.velocity = Vector2()
         self.rect = pygame.FRect(self._image.get_rect())
         self.rect.center = position
 
-        self.score = 0
-
     def update(self, dt: float):  # type: ignore
-        self.velocity *= 1 - MEOW_VELOCITY_DAMPING
         self.rect = self.rect.move(self.velocity * dt)  # type: ignore
 
     def show(self, surface: pygame.Surface):
